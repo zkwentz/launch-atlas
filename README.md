d00e5dc7ac31fe61a223ff1565d4698d5af50015
Zach Wentz <zkwentz@gmail.com>
2019-03-21 22:39:20 -0500

    Auto finds connected Arduino

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Supporting script for Arduino device to click the launch button when switch is a
 
 ## TODO
 
-- [] Dynamically determine the right port to listen to serial stream.
+- [x] Dynamically determine the right port to listen to serial stream.
--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const robot = require('robotjs');
-const SerialPort = require('serialport')
+const SerialPort = require('serialport');
 const portName = '/dev/cu.usbmodem14101';
-const port = new SerialPort(portName, { 
-  baudRate: 9600
+
+SerialPort.list(function(err, ports) {
+  var allports = ports.length;
+  var count = 0;
+  var done = false;
+  ports.forEach(function(port) {
+    count += 1;
+    pm = port['manufacturer'];
+    if (typeof pm !== 'undefined' && pm.includes('arduino')) {
+      console.log('Found arduino, listening...');
+      const sp = new SerialPort(port.comName.toString(), {
+        baudRate: 9600
+      });
+
+      sp.on('data', function (data) {
+        console.log('Data:', data.toString());
+        robot.mouseClick();
+      });
+      done = true;
+    }
+    if (count === allports && done === false) {
+      console.log('cant find arduino');
+      return portName;
+    }
+  });
 });
 
-port.on('data', function (data) {
-  console.log('Data:', data.toString());
-  robot.mouseClick();
-});
+
+
